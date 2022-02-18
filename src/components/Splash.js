0fdbb6bf20ca88fe9ad0c3bbb3a583dceab74ba8
Tig-ranK <karakhanyantigran@outlook.com>
2022-02-18 16:24:14 +0400

    clean boilerplate, create Splash component

---
+++ src/components/Splash.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Splash() {
+  return (
+    <div className='splash'>
+      <h1>Quizzical</h1>
+      <p>Some description if needed</p>
+      <button>Start Quiz</button>
+    </div>
+  );
+}
