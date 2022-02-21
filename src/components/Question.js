47cc84a1708d867a166710293681e9b9d253a640
Tig-ranK <karakhanyantigran@outlook.com>
2022-02-21 17:09:42 +0400

    added styles for selected answers

--- src/components/Question.js
+++ src/components/Question.js
@@ -6,7 +6,7 @@ export default function Question(props) {
       <h2 className='question-title'>How would one say goodbye in Spanish?</h2>
       <div className="question-btn-wrapper">
         <button className='button light'>Adios</button>
-        <button className='button light'>Hola</button>
+        <button className='button light selected'>Hola</button>
         <button className='button light'>Au Revoir</button>
         <button className='button light'>Salir</button>
       </div>
