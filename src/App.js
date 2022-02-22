8f499e4816139e51b637995ddf30dd9a956212d1
Tig-ranK <karakhanyantigran@outlook.com>
2022-02-22 16:00:34 +0400

    added logic for picking an answer, connected to API

--- src/App.js
+++ src/App.js
@@ -4,18 +4,43 @@ import Question from './components/Question';
 import { nanoid } from 'nanoid';
 import './styles.scss';
 
+async function fetchData(url) {
+  const response = await fetch(url);
+  const json = await response.json();
+  return json.results;
+}
+
 export default function App() {
-  const [selected, setSelected] = useState([1, 4, 2, 3, 1]);
-  const [start, setStart] = useState(true);
+  const [selected, setSelected] = useState([0,0,0,0,0]);
+  const [start, setStart] = useState(false);
+  const [data, setData] = useState([])
   function handleClick() {
     setStart((prev) => !prev);
   }
-  
-  useEffect(() => {}, []);
 
-  const questionArray = selected.map((elem) => (
-    <Question selected={elem} id={nanoid()} />
+  useEffect(() => {
+    const url =
+      'https://opentdb.com/api.php?amount=5&difficulty=medium&type=multiple';
+    fetchData(url).then(setData);
+  }, []);
+
+  function selectAnswer(questionNumber, answerNumber) {
+    setSelected((prev) => {
+      let copy = prev.slice();
+      copy[questionNumber] = answerNumber;
+      return copy;
+    });
+  }
+  const questionArray = selected.map((elem, index) => (
+    <Question
+    content={data[index]}
+      key={nanoid()}
+      index={index}
+      selected={elem}
+      selectAnswer={selectAnswer}
+    />
   ));
+
   return (
     <div className='container'>
       {start ? <>{questionArray}</> : <Splash handleClick={handleClick} />}
--- src/components/Question.js
+++ src/components/Question.js
@@ -1,15 +1,31 @@
 import React from 'react';
 
 export default function Question(props) {
+  const content = {
+    category: props.content.category,
+    difficulty: props.content.difficulty,
+    question: props.content.question,
+    answers: [...props.content.incorrect_answers, props.content.correct_answer],
+  };
+  console.log(content)
+  const answersArray = content.answers.map(
+    (answer, idx) => (
+      <button
+        key={idx}
+        className={
+          idx === props.selected ? 'button light selected' : 'button light'
+        }
+        onClick={() => props.selectAnswer(props.index, idx)}
+      >
+        {answer}
+      </button>
+    )
+  );
+
   return (
     <div className='question'>
-      <h2 className='question-title'>How would one say goodbye in Spanish?</h2>
-      <div className="question-btn-wrapper">
-        <button className='button light'>Adios</button>
-        <button className='button light selected'>Hola</button>
-        <button className='button light'>Au Revoir</button>
-        <button className='button light'>Salir</button>
-      </div>
+      <h2 className='question-title'>{content.question}</h2>
+      <div className='question-btn-wrapper'>{answersArray}</div>
       <hr />
     </div>
   );
